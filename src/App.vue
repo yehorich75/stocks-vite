63274789095eb1ac05be0983296b00a8fd7b1d7b
Yegorich75 <yegorich75@gmail.com>
2021-06-15 09:44:29 +0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <h2 class="title">Stocks App</h2>
+    <Stocks />
+  </div>
+</template>
+
+<script lang="ts">
+import { ref, defineComponent } from 'vue'
+import Stocks from './components/Stocks.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Stocks
+  }
+})
+</script>
---
+++ src/components/Stocks.vue
@@ -0,0 +1,9 @@
+<template>
+<div class="stock">
+  <div class="stock-item"></div>
+</div>
+</template>
+
+<script lang="ts">
+</script>
+
