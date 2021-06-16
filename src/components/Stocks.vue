2244542b0d65b76907c9f9471beb3b8d924cdbff
yegorich75 <yegorich75@gmail.com>
2021-06-16 07:01:20 +0300

    axios start

--- src/components/Stocks.vue
+++ src/components/Stocks.vue
@@ -1,7 +1,18 @@
 <template>
-<div class="stock">
-  <div class="stock-item"></div>
-</div>
+  <div class="stock">
+    <div class="stock-item">
+      <div class="stock-item__info">
+        <div class="stock-item__cover">
+          <img src="../assets/logo.png" alt="Logo Company">
+        </div>
+        <h3 class="stock-item__title">
+          Apple Inc
+          <span>AAPL</span>
+        </h3>
+      </div>
+      <span class="stock-item__price">127.35 $</span>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
